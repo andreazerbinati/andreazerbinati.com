c7c8583346148b7cbb3106363a9f4d8df7bd6f36
andreazerbinati <zeran23@gmail.com>
2019-08-14 15:24:49 +0200

    new logo and metatags

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,36 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `Andrea Zerbinati`,
+    name: `Andrea Zerbinati`,
+    siteUrl: `https://andreazerbinati.com`,
+    description: `Il primo blog italiano su DataScience.`,
     hero: {
       heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
       maxWidth: 652,
     },
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/narative`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
-    ],
-  },
+  //   social: [
+  //     {
+  //       name: `twitter`,
+  //       url: `https://twitter.com/narative`,
+  //     },
+  //     {
+  //       name: `github`,
+  //       url: `https://github.com/narative`,
+  //     },
+  //     {
+  //       name: `instagram`,
+  //       url: `https://instagram.com/narative.co`,
+  //     },
+  //     {
+  //       name: `linkedin`,
+  //       url: `https://www.linkedin.com/company/narative/`,
+  //     },
+  //     {
+  //       name: `dribbble`,
+  //       url: `https://dribbble.com/narativestudio`,
+  //     },
+  //   ],
+  // },
   plugins: [
     {
       resolve: "@narative/gatsby-theme-novela",
